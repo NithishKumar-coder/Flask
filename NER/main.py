45796d17ae37c5dbed97c28a18cb9e1b88d1ef67
Nithish Kumar <54560557+NithishKumar-coder@users.noreply.github.com>
2020-07-04 01:02:49 +0530

    Add files via upload

---
+++ NER/main.py
@@ -0,0 +1,32 @@
+import pos_tagger  #create module 
+import os
+import sys
+import argparse
+
+
+def com(url,folder_name):
+    if "http" or "www" in url:
+        pos_tagger.send(url,folder_name)         
+    else:
+        print("You have entered an invalid url")
+
+#Function to return usage message 
+def msg():
+    message='''You failed to provide url as input on command line'''
+    return message
+    
+#Creating a parser
+parser = argparse.ArgumentParser(usage=msg())
+#Adding arguments
+parser.add_argument("\"You must provide url on command line\"")
+#Parsing arguments
+args = parser.parse_args()
+
+#creating new  data folder directory
+new_path = r'F:\j\ner' 
+if not os.path.exists(new_path):
+    os.makedirs(new_path)
+folder_name="data"
+print("Directory '% s' is built!!!!\n" % new_path)
+
+com(sys.argv[1],folder_name)
